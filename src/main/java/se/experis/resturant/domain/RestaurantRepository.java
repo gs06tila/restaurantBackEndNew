40e09ea5be6f6b875757554af65abe834cb34a52
gs06tila <tim93larsson@gmail.com>
2019-10-16 20:26:36 +0200

    fix
    coding coding coding

--- src/main/java/se/experis/resturant/domain/RestaurantRepository.java
+++ src/main/java/se/experis/resturant/domain/RestaurantRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface git RestaurantRepository extends CrudRepository<Restaurant, Long> {
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
     //Fetch restaurants by category
     //List<Restaurant> findByCategory(String category);
 
